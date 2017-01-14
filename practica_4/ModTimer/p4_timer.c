03abb038735254b8fe77500e9001ae464d9add6c
Fuytetcei <david.daniel.abellan@gmail.com>
2017-01-14 12:02:28 +0100

    p4 avanzada

--- practica_4/ModTimer/p4_timer.c
+++ practica_4/ModTimer/p4_timer.c
@@ -1,4 +1,5 @@
 
+
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/proc_fs.h>
@@ -6,28 +7,88 @@
 #include <linux/timer.h>
 #include <linux/random.h>
 #include <linux/fs.h>
-
+#include <linux/workqueue.h>
 #include "cbuffer.h"
 
-struct timer_list my_timer; /* Structure that describes the kernel timer */
+// Estructura del timer
+struct timer_list my_timer;
 
+// Buffer circulas
 cbuffer_t *buff;
 
+// Lista enlazada
+struct list_head mylist;
+
+typedef struct {
+    int data;
+    struct list_head links;
+}list_item_t;
+
+// Datos de configuración
 int timer_period_ms;
 int max_randoms;
 int emergency_threshold;
 
+list_item_t nodes[max_randoms];
+int flag;
 
-/* Function invoked when timer expires (fires) */
-static void fire_timer(unsigned long data)
-{
+// Workqueue
+struct workqueue_struct myqueue;
+
+// Mutex para sincronización
+DEFINE_SPINLOCK(mtxCBuff);
+DEFINE_SPINLOCK(mtxList);
+
+// Semáforo
+// struct semaphore mysem;
+
+// Función de workqueue
+void bottom_half (struct work_struct *work) {
+    int _data, i;
+
+    // Leo del buffer
+    spin_lock(&mtxCBuff);
+
+    i=0;
+    while(!is_empty_cbuffer_t(buff)) {
+
+        // Extraigo elemento
+        remove_items_cbuffer_t(buff, (char*) &_data, 4);
+
+        // Asigno datos
+        nodes[flag].data = data;
+
+        // Escribo en lista enlazada
+        spin_lock(&mtxList);
+            // Espero a que se consuman los elementos
+            down(&mysem);
+                list_add_tail(&nodes[i].links, &mylist);
+        spin_unlock(&mtxList);
+        flag += (flag)%max_randoms;
+    }
+
+    // Despierto al programa de usuario
+    up(&mysem);
+
+    spin_unlock(&mtxCBuff);
+
+    // Planifico timer de nuevo
+    mod_timer( &(my_timer), jiffies + HZ);
+}
+
+
+// Función del timer
+static void fire_timer(unsigned long data) {
     unsigned int ran;
     int cpu;
+    struct work_struct *newwork;
 
     // Genero un número aleatorio
     ran = get_random_int();
 
     // Escribo en el buffer circular
+    /*************** SECCION CRITICA *******************/
+    spin_lock(&mtxCBuff);
     insert_items_cbuffer_t(buff, (char*) &ran, 4);
 
     // Difiero tarea
@@ -41,17 +102,18 @@ static void fire_timer(unsigned long data)
         else
             cpu = cpu++;
 
-
-
+        INIT_WORK(newwork, bottom_half);
+        queue_work_on(cpu, &myqueue, newwork);
     }
         
     // Miro si debo reactivar el timer
     if(!is_full_cbuffer_t(buff))
-	   mod_timer( &(my_timer), jiffies + HZ); 
+	   mod_timer( &(my_timer), jiffies + HZ);
+    spin_unlock(&mtxCBuff);
+    /************** FIN SECCION CRITICA *****************/
 }
 
-int init_timer_module( void )
-{
+int init_timer_module( void ) {
     struct file *fd = NULL;
 
     // Leo los parámetros de configuración
@@ -61,13 +123,23 @@ int init_timer_module( void )
         return -1;
     }
 
-    /***************************************************************/
+    /********************** CARGAR CONFIGURACION ************************/
 
     // Creo el buffer
     buff = create_cbuffer_t(max_randoms*4);
 
     // Creo el timer
     init_timer(&my_timer);
+
+    // Creo workqueue
+    &myqueue = create_workqueue("p4");
+
+    // Creo la lista
+    INIT_LIST_HEAD(&mylist);
+    flag=0;
+
+    // Inicializo semáforo
+    sema_init(&mysem, 0);
 
     // Inicializo el timer
     my_timer.data=0;
@@ -81,11 +153,15 @@ int init_timer_module( void )
 
 
 void cleanup_timer_module( void ){
-  // Espero a que acabe el timer y lo apago
-  del_timer_sync(&my_timer);
+    // Destruyo workqueue
+    flush_workqueue(&myqueue);
+    destroy_workqueue(&myqueue);
 
-  // Destruyo el buffer ciercular
-  destroy_cbuffer_t(buff);
+    // Espero a que acabe el timer y lo apago
+    del_timer_sync(&my_timer);
+
+    // Destruyo el buffer ciercular
+    destroy_cbuffer_t(buff);
 
 }
 
